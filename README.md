8f78ff2ec62426c20d6044ce6dda14991a5ceb71
Greg Bowler <greg.bowler@g105b.com>
2023-02-16 19:13:33 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<img src="http://159.65.210.101/php-actions.png" align="right" alt="PHP Actions for Github" />
+
+Run PHP Mess Detector tests in Github Actions.
+==============================================
+
+PHP Mess Detector (PHPMD) takes a given PHP source code base and looks for several potential problems within that source. These problems can be things like:
+
++ Possible bugs
++ Suboptimal code
++ Overcomplicated expressions
++ Unused parameters, methods, properties
+
+Usage
+-----
+
+// TODO
---
+++ phpmd-action.bash
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+github_action_path=$(dirname "$0")
+docker_tag=$(cat ./docker_tag)
+
+if [ -z "$ACTION_PHPMD_PATH" ]
+then
+	phar_url="https://www.getrelease.download/phpmd/phpmd/$ACTION_VERSION/phar"
+	phar_path="${github_action_path}/phpmd.phar"
+	curl --silent -H "User-agent: cURL (https://github.com/php-actions)" -L "$phar_url" > "$phar_path"
+else
+	phar_path="${GITHUB_WORKSPACE}/$ACTION_PHPMD_PATH"
+fi
+
+chmod +x $phar_path
+command_string=("phpmd")
+
+if [ -n "$ACTION_PATH" ]
+then
+	command_string+=("$ACTION_PATH")
+fi
+
+if [ -n "$ACTION_OUTPUT" ]
+then
+	command_string+=("$ACTION_OUTPUT")
+fi
+
+if [ -n "$ACTION_RULESET" ]
+then
+	command_string+=("$ACTION_RULESET")
+fi
+
+if [ -n "$ACTION_MINIMUMPRIORITY" ]
+then
+	command_string+=(--minimumpriority "$ACTION_MINIMUMPRIORITY")
+fi
+
+if [ -n "$ACTION_REPORTFILE" ]
+then
+	command_string+=(--reportfile "$ACTION_REPORTFILE")
+fi
+
+if [ -n "$ACTION_SUFFIXES" ]
+then
+	command_string+=(--suffixes "$ACTION_SUFFIXES")
+fi
+
+if [ -n "$ACTION_EXCLUDE" ]
+then
+	command_string+=(--exclude "$ACTION_EXCLUDE")
+fi
+
+if [ -n "$ACTION_STRICT" ]
+then
+	command_string+=(--strict)
+fi
+
+if [ -n "$ACTION_ARGS" ]
+then
+	command_string+=($ACTION_ARGS)
+fi
+
+echo "Command: ${command_string[@]}" >> output.log 2>&1
+
+docker run --rm \
+	--volume "${phar_path}":/usr/local/bin/phpmd \
+	--volume "${GITHUB_WORKSPACE}":/app \
+	--workdir /app \
+	--network host \
+	--env-file <( env| cut -f1 -d= ) \
+	${docker_tag} "${command_string[@]}"
